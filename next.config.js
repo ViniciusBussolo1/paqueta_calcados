01b27271fd893b49d284193a756d71573f9932fd
Vinicius Bussolo <vinicius.buss13@gmail.com>
2023-06-16 14:59:04 -0300

    created carousel for shoes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.brchallenges.com',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
--- src/components/Outlet/Outlet.tsx
+++ src/components/Outlet/Outlet.tsx
@@ -1,16 +1,20 @@
 'use client'
 
 import { useQuery } from '@tanstack/react-query'
-import { useKeenSlider } from 'keen-slider/react'
-import api from '@/services/api'
+
 import Image from 'next/image'
+import HeartOrange from '../../assets/icons/heart-orange.svg'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper'
+
+import api from '@/services/api'
 
 import ButtonNumbers from './ButtonNumbers'
-
 import Logo from '../../assets/logo.png'
 
-import 'keen-slider/keen-slider.min.css'
-import { useState } from 'react'
+import 'swiper/css/pagination'
+import 'swiper/css'
 
 interface dataShoesProps {
   id: string
@@ -25,23 +29,6 @@ interface dataShoesProps {
 }
 
 export default function Outlet() {
-  const [currentSlide, setCurrentSlide] = useState(0)
-  const [loaded, setLoaded] = useState(false)
-
-  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
-    initial: 0,
-    slides: {
-      perView: 4,
-      spacing: 15,
-    },
-    slideChanged(slider) {
-      setCurrentSlide(slider.track.details.rel)
-    },
-    created() {
-      setLoaded(true)
-    },
-  })
-
   const getShoes = async () => {
     return await api.get('/shoes')
   }
@@ -83,7 +70,7 @@ export default function Outlet() {
         <ButtonNumbers number={43} />
         <ButtonNumbers number={44} />
       </div>
-      <div className="w-[82.071rem] mt-[10.5rem]">
+      <div className="w-[82.071rem] mt-[10.5rem] ">
         <div className="w-full flex justify-between items-center">
           <h2 className="font-semibold text-[2.125rem] leading-[3rem] text-black-800 uppercase">
             Destaques
@@ -95,48 +82,100 @@ export default function Outlet() {
             <div className="absolute left-0 w-0 h-[0.063rem] bg-orange-600 transition-all group-hover:w-full"></div>
           </div>
         </div>
-        <div className="navigation-wrapper">
-          <div ref={sliderRef} className="keen-slider h-96 ">
-            <div className="keen-slider__slide number-slide1 bg-red-500">1</div>
-            <div className="keen-slider__slide number-slide2 bg-blue-500">
-              2
-            </div>
-            <div className="keen-slider__slide number-slide3 bg-green-500">
-              3
-            </div>
-            <div className="keen-slider__slide number-slide4 bg-yellow-500">
-              4
-            </div>
-          </div>
-          {loaded && instanceRef.current && (
-            <div className="flex justify-center gap-2 py-[0.625rem]">
-              {[
-                ...Array(
-                  instanceRef.current.track.details.slides.length,
-                ).keys(),
-              ].map((idx) => {
-                return (
-                  <button
-                    key={idx}
-                    onClick={() => {
-                      instanceRef.current?.moveToIdx(idx)
-                    }}
-                    className={`dot ${
-                      currentSlide === idx ? 'active' : ''
-                    } border-none w-3 h-3 bg-[#c5c5c5] rounded-[50%] px-[0.313rem] py-[0.313rem] cursor-pointer focus:outline-none`}
-                  ></button>
-                )
-              })}
-            </div>
-          )}
-        </div>
+
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="h-[30rem] mt-12"
+        >
+          {data?.data.map((shoe: dataShoesProps) => {
+            return (
+              <SwiperSlide key={shoe.id}>
+                {shoe.soldout === true ? (
+                  <>
+                    <div className="w-full bg-orange-600 rounded-t-[0.188rem] flex justify-center items-center py-2">
+                      <span className="text-sm font-bold text-white uppercase">
+                        produto esgotado
+                      </span>
+                    </div>
+                    <div className="w-[19.188rem] h-[25.75rem] px-6 pt-9 pb-6 flex flex-col items-center justify-end relative">
+                      <Image
+                        src={HeartOrange}
+                        alt="Icon Heart"
+                        className="absolute top-[11%] right-[10%] cursor-pointer"
+                      />
+                      <div>
+                        <Image
+                          src={shoe.image}
+                          alt={shoe.name}
+                          width={210}
+                          height={149}
+                        />
+                      </div>
+                      <div className="w-full flex flex-col gap-3 mb-3">
+                        <span className="font-medium text-base text-black-800">
+                          {shoe.name}
+                        </span>
+                        <div className="flex flex-col gap-1">
+                          <span className="font-bold text-xl text-black-800">
+                            R$ {shoe.price.value}
+                          </span>
+                          <span className="text-xs text-black-400 opacity-60 uppercase">
+                            ou 10x de{' '}
+                            {parseFloat((shoe.price.value / 9).toFixed(2))}
+                          </span>
+                        </div>
+                      </div>
+
+                      <button className="w-full bg-orange-600 font-semibold text-base uppercase text-white rounded-sm py-3 ">
+                        me avise quando chegar
+                      </button>
+                    </div>
+                  </>
+                ) : (
+                  <div className="w-[19.188rem] h-[28rem] px-6 pt-9 pb-6 flex flex-col items-center justify-end relative">
+                    <Image
+                      src={HeartOrange}
+                      alt="Icon Heart"
+                      className="absolute top-[11%] right-[10%] cursor-pointer"
+                    />
+                    <div>
+                      <Image
+                        src={shoe.image}
+                        alt={shoe.name}
+                        width={210}
+                        height={149}
+                      />
+                    </div>
+                    <div className="w-full flex flex-col gap-3 mb-3">
+                      <span className="font-medium text-base text-black-800">
+                        {shoe.name}
+                      </span>
+                      <div className="flex flex-col gap-1">
+                        <span className="font-bold text-xl text-black-800">
+                          R$ {shoe.price.value}
+                        </span>
+                        <span className="text-xs text-black-400 opacity-60 uppercase">
+                          ou 10x de{' '}
+                          {parseFloat((shoe.price.value / 9).toFixed(2))}
+                        </span>
+                      </div>
+                    </div>
+
+                    <button className="w-full bg-linear-gradient-button font-bold text-lg uppercase text-white rounded-sm py-2">
+                      comprar
+                    </button>
+                  </div>
+                )}
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
       </div>
     </section>
   )
 }
-
-// {
-// data?.data.map((shoe: dataShoesProps) => {
-//   return <h1 key={shoe.id}>{shoe.name}</h1>
-// })
-// }
